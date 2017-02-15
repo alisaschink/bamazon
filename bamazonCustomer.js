c7c64778a821d2e072e988b0dd58d1e844ce53ac
Alisa Schink <Alisa Schink>
2017-02-15 17:26:24 -0500

    colors and cli-table npm packages added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
+var colors = require('colors');
 
 //connects to bamazon database
 var connection = mysql.createConnection({
@@ -14,111 +16,91 @@ connection.connect();
 //logs product info
 connection.query('SELECT * FROM products', function (error, results, fields)
 {
-  console.log(results);
-  console.log('\n');
+  var table = new Table({
+            head: ['Id'.yellow.bold, 'Product Name'.yellow.bold, 'Price'.yellow.bold],
+        });
+        console.log('Products Available:'.cyan.bold);
+        for (var i = 0; i < results.length; i++) {
+            table.push([results[i].id, results[i].product_name, results[i].price]);
+        }
+        console.log(table.toString());
+         console.log('\n');
 
   // asks user if they would like to make a purchase
   inquirer.prompt([
     {type: "input",
       name: "purchase",
-      message: "Would you like to make a purchase?"}
+      message: "Would you like to make a purchase (yes/no)?".yellow.bold}
       ]).then(function(data){
         if (data.purchase == 'yes') {
-          // asks user what produce they would like to purchase
-          inquirer.prompt([
-          {type: "input",
-          name: "product_id",
-          message: "What is the id of the product you would like to purchase?"}
-          ]).then(function(data){
-            var product = data.product_id;
-            //asks for quantity they would like to purchase
-            inquirer.prompt([
-              {type: "input",
-              name: "units",
-              message: "What is quantity that you would like to purchase?"}
-              ]).then(function(data){
-                var units = data.units;
-                var item = results[product - 1];
-                var total = units * (item.price);
-                var newStock = item.stock_quantity - units;
-                // logs a summary of what is being purchased
-                console.log("you are purchasing " + units + " " + item.product_name + "s");
-                // checks if there is sufficient quantity left in stock for sale
-                if(item.stock_quantity >= units){
-                  // updates stock quantity after sale
-                  connection.query("UPDATE products SET stock_quantity=" + newStock + " WHERE id=" + item.id, function(err, res) { 
-                    if (err) return console.log(err);
-                      console.log('update completed!')
-                  });
-
-                  // adds sale to sales table
-                  connection.query("INSERT INTO sales (product_id, quantity_purchased) VALUES (" + item.id + ", " + units + " )", function(err, res) { 
-                    if (err) return console.log(err);
-                      console.log('sale has been recorded')
-                  });
-
-                  var total = units * (item.price);
-                  console.log("purchase approved");
-                  console.log("Your sales total today is " + total + " dollars");
-                  console.log("there are " + newStock + " " + item.product_name + "s left");
-                } else{
-                  console.log("Insufficient quantity! Try again.");
-                }
-  
-              });
-
-          });
-          
-          
-      } else{
+          newPurchase();
+        } else{
           console.log("come back when you want to spend money");
           connection.end();
         }
 
   });
 
+  function newPurchase(){
+    // asks user what produce they would like to purchase
+    inquirer.prompt([{
+      type: "input",
+      name: "product_id",
+      message: "What is the id of the product you would like to purchase?"}
+    ]).then(function(data){
+      var product = data.product_id;
+      //asks for quantity they would like to purchase
+      inquirer.prompt([{
+        type: "input",
+        name: "units",
+        message: "What is quantity that you would like to purchase?"}
+      ]).then(function(data){
+        var units = data.units;
+        var item = results[product - 1];
+        var total = units * (item.price);
+        var newStock = item.stock_quantity - units;
+
+        // logs a summary of what is being purchased
+        console.log((" you are purchasing " + units + " " + item.product_name + "s").magenta.bold);
+
+        // checks if there is sufficient quantity left in stock for sale
+        if(item.stock_quantity >= units){
+          // updates stock quantity after sale
+          connection.query("UPDATE products SET stock_quantity=" + newStock + " WHERE id=" + item.id, function(err, res) { 
+            if (err) return console.log(err);
+          });
+
+          // adds sale to sales table
+          connection.query("INSERT INTO sales (product_id, quantity_purchased) VALUES (" + item.id + ", " + units + " )", function(err, res) { 
+            if (err) return console.log(err);
+          });
+
+          var total = units * (item.price);
+          console.log(" purchase approved!".green.bold);
+          console.log(" ----------------------------------------".green.bold);
+          console.log((" | Your sales total today is " + total + " dollars |").green.bold);
+          console.log(" ----------------------------------------".green.bold);
+          inquirer.prompt([{
+            type: "input",
+            name: "new_purchase",
+            message: "Would you like to continue shopping (yes/no)?".yellow.bold}
+          ]).then(function(data){
+            if (data.new_purchase == 'yes') {
+              newPurchase();
+            } else{
+              console.log("Thank you for Shopping! Have a nice day!");
+              connection.end();
+            }
+          });
+        } else{
+          console.log("Insufficient quantity! Try again.".red.bold);
+        }
+  
+      });
+
+    });
+  }
+
 });
 
- 
-
-
-
-
-
-
-// function insertIntoTable(name, type, abv, table){
-//   connection.query("INSERT INTO " + table + " SET ?", {
-//       name: name,
-//       type: type,
-//       abv: abv
-//     }, function(err, res) { console.log('completed!')});
-// }
-
-// function deleteFromTable(id, table){
-//  connection.query("DELETE FROM " + table + " WHERE ?", {
-//      id: id
-//    }, function(err, res) { 
-//      if (err) return console.log(err);
-//      console.log('delete completed!')
-//    });
-// }
-
-// //write update function
-// function updateTable(id, table){
-//  connection.query("UPDATE " + table + " SET ? WHERE ?", [{
-//    name : 'bruno beer'
-//    }, {
-//      id : id
-//    }], function(err, res) { 
-//      if (err) return console.log(err);
-//      console.log('update completed!')
-//    });
-// }
-
-// //write delete function
-
-
-// // insertIntoTable('beer', 'i dont know beer', 100, 'beers');
-// // deleteFromTable(7, 'beers');
-// updateTable(1, 'beers');
 
